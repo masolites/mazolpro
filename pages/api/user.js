195a8812f722be9f1a01ffdfc9995ac116cc82e9
masolites <mfmmpcs@gmail.com>
2025-07-05 10:04:41 +0100

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,4 +1,4 @@
- import { connectToDatabase } from "../../lib/mongodb";
+import { connectToDatabase } from "../../lib/mongodb";
 
 export default async function handler(req, res) {
   // For demo, return test user data
