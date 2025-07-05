d05018e2087c9965c9de10b84cea983707ea8f7c
masolites <mfmmpcs@gmail.com>
2025-07-05 05:20:23 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
   reactStrictMode: true,
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
+    FLW_PUBLIC_KEY: process.env.FLW_PUBLIC_KEY,
+    FLW_SECRET_KEY: process.env.FLW_SECRET_KEY,
     THIRDWEB_CLIENT_ID: process.env.THIRDWEB_CLIENT_ID,
     TOKEN_CONTRACT: process.env.TOKEN_CONTRACT
   }
