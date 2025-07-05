e5e8f8eb7635490b1d2262e838b307d3d1b63a2a
masolites <mfmmpcs@gmail.com>
2025-07-05 07:36:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    FLW_PUBLIC_KEY: process.env.FLW_PUBLIC_KEY,
-    FLW_SECRET_KEY: process.env.FLW_SECRET_KEY,
-    THIRDWEB_CLIENT_ID: process.env.THIRDWEB_CLIENT_ID,
-    TOKEN_CONTRACT: process.env.TOKEN_CONTRACT
-  },
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
+  images: { domains: ["res.cloudinary.com"] },
 };
