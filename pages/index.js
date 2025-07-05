e7d85c78dee1ffccbdbbd7275a50bb40c2494270
masolites <mfmmpcs@gmail.com>
2025-07-05 07:38:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import Dashboard from '../components/Dashboard';
-import Head from 'next/head';
+import { ThirdwebProvider } from "thirdweb/react";
+import Dashboard from "../components/Dashboard";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <ThirdwebProvider 
-      activeChain="binance" 
-      clientId={process.env.THIRDWEB_CLIENT_ID}
+    <ThirdwebProvider
+      activeChain="binance"
+      clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
     >
       <Head>
-        <title>MASOL E-COMMERCE</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Mazol E-commerce</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
       </Head>
       <Dashboard />
     </ThirdwebProvider>
