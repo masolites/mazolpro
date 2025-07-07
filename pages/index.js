dc6ce7bf3952ad3fec5986f789a157b588576dd4
masolites <mfmmpcs@gmail.com>
2025-07-07 21:14:02 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "5rem" }}>
+      <h1>MAZOL-Pro: E-commerce & Blockchain</h1>
+      <p>Welcome to your production-ready platform.</p>
+    </div>
+  );
+}
