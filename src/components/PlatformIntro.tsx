b92a114385b27ebd7c7e7d9bdb1796f5f0bf2c8b
masolites <mfmmpcs@gmail.com>
2025-07-09 15:42:26 +0100

    Create PlatformIntro.tsx

---
+++ src/components/PlatformIntro.tsx
@@ -0,0 +1,11 @@
+export default function PlatformIntro() {
+  return (
+    <section className="intro">
+      <h2>
+        Mazol-Pro is a Blockchain supported Platform
+        promoting a Better Society Together by offering
+        Trusted Systems, Goods & Services to people
+      </h2>
+    </section>
+  );
+}
