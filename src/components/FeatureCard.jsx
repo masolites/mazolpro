661801762bfcbe85abf9f9ceedf2232bf6a11490
masolites <mfmmpcs@gmail.com>
2025-07-10 23:35:19 +0100

    Update FeatureCard.jsx

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,21 +1,17 @@
-import { Box, Text } from "@chakra-ui/react";
+ import { Box } from "@chakra-ui/react";
 
-export default function FeatureCard({ title, children }) {
+export default function FeatureCard({ children }) {
   return (
     <Box
       bg="cream.100"
-      color="maroon.700"
+      color="maroon.800"
       borderRadius="lg"
       boxShadow="lg"
       p={6}
       mb={6}
       minH="220px"
     >
-      <Text fontWeight="bold" fontSize="xl" mb={4}>
-        {title}
-      </Text>
       {children}
     </Box>
   );
 }
-
