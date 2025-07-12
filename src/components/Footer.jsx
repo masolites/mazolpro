6e8124f5aac8b2576dc58ca092750f6eca93250b
masolites <mfmmpcs@gmail.com>
2025-07-12 10:30:54 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,10 @@
- import { Box, Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
-    <Box
-      as="footer"
-      py={4}
-      textAlign="center"
-      bg="maroon.900"
-      color="cream.100"
-      mt={8}
-    >
+    <Box as="footer" py={4} textAlign="center" bg="maroon.900" color="cream.100" mt={8}>
       <Text>
-        Admin Dashboard © {new Date().getFullYear()} Mazol
-        Pro
+        Admin Dashboard © {new Date().getFullYear()} Mazol Pro
       </Text>
     </Box>
   );
