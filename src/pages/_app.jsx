2376ea01bb6f223f83173109ff8d5a367f7facf1
masolites <mfmmpcs@gmail.com>
2025-07-12 06:30:16 +0100

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,26 @@
+ // pages/_app.jsx
+
+import { useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "../contexts/AuthContext";
+import { ThirdwebProvider } from "thirdweb/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import theme from "../styles/theme";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  // Create a QueryClient instance only once
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ChakraProvider theme={theme}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThirdwebProvider>
+        <ChakraProvider theme={theme}>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </ChakraProvider>
+      </ThirdwebProvider>
+    </QueryClientProvider>
   );
 }
