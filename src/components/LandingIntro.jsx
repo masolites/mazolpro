00e8bcdb00ada638c61e951dabb34b437a4a2260
masolites <mfmmpcs@gmail.com>
2025-07-12 10:27:20 +0100

    Update LandingIntro.jsx

--- src/components/LandingIntro.jsx
+++ src/components/LandingIntro.jsx
@@ -1,30 +1,22 @@
- import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Text, Heading } from "@chakra-ui/react";
 
 export default function LandingIntro() {
   return (
-    <Box
-      bg="maroon.700"
-      color="cream.100"
-      borderRadius="lg"
-      boxShadow="lg"
-      p={{ base: 3, md: 4 }}
-      mb={4}
-      textAlign="center"
-      maxW="400px"
-      mx="auto"
-    >
-      <Heading fontSize="2xl" mb={2}>
-        Welcome to Mazol Pro
+    <Box textAlign="center" py={8} px={4}>
+      <Heading color="turquoise.500" mb={2} textShadow="1px 2px 8px #000">
+        Mazol Pro E-commerce & Blockchain
       </Heading>
       <Text
-        fontSize={{ base: "md", md: "lg" }} // matches subtitle in header
-        color="cream.200"
-        maxW="350px"
+        fontSize={{ base: "lg", md: "xl" }}
+        color="cream.100"
+        maxW="600px"
         mx="auto"
+        bg="maroon.700"
+        p={4}
+        borderRadius="xl"
+        boxShadow="0 2px 8px 0 #fff5e122"
       >
-        Mazol-Pro is a Blockchain supported Platform
-        promoting a Better Society Together by offering
-        Trusted Systems, Goods & Services to people.
+        Mazol-Pro is a Blockchain supported Platform promoting a Better Society Together by offering Trusted Systems, Goods & Services to people.
       </Text>
     </Box>
   );
