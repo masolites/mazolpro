ca4763cc747cc14f676b8dfd3e3289ba202deb8d
masolites <mfmmpcs@gmail.com>
2025-07-14 12:16:20 +0100

    Update mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,1 +1,10 @@
+// Example stub for MongoDB connection
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+let client;
+export default async function dbConnect() {
+  if (!client) client = new MongoClient(uri);
+  if (!client.isConnected()) await client.connect();
+  return client.db();
+}
 
