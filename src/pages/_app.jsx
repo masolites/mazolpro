3c9fa322fcbe256aeb7e3df16f532bcb090d9b6b
masolites <mfmmpcs@gmail.com>
2025-07-15 00:25:55 +0100

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,6 @@
- import { ChakraProvider } from "@chakra-ui/react";
+ // pages/_app.js
+
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import { ThirdwebProvider } from "thirdweb/react";
 import { AuthProvider } from "../contexts/AuthContext";
@@ -7,8 +9,8 @@ export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <ThirdwebProvider
-        clientId="71e20f4fe4537525ee7c766d094b27b1" // Your actual thirdweb clientId
-        activeChain="binance" // BEP-20 (Binance Smart Chain)
+        clientId="71e20f4fe4537525ee7c766d094b27b1"
+        activeChain="binance"
       >
         <AuthProvider>
           <Component {...pageProps} />
