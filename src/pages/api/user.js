d2009c7a0f2eec260a79cc842084993c479b190f
masolites <mfmmpcs@gmail.com>
2025-07-14 19:21:23 +0100

    Create user.js

---
+++ src/pages/api/user.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+module.exports = async (req, res) => {
+  let client;
+  try {
+    client = await MongoClient.connect(uri);
+    const db = client.db();
+    const { wallet } = req.body;
+
+    if (!wallet) {
+      return res
+        .status(400)
+        .json({ error: "Wallet address required." });
+    }
+
+    let user = await db
+      .collection("users")
+      .findOne({ wallet });
+    if (!user) {
+      await db.collection("users").insertOne({
+        wallet,
+        email: "",
+        pin: "",
+        nairaBalance: 0,
+        usdtBalance: 0,
+        createdAt: new Date(),
+      });
+      user = await db
+        .collection("users")
+        .findOne({ wallet });
+    }
+
+    res.status(200).json({ user });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Server error: " + err.message });
+  } finally {
+    if (client) client.close();
+  }
+};
