796efd4482a32703f4cb1a73bd434bb175db37ce
masolites <mfmmpcs@gmail.com>
2025-07-17 17:44:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import { useState } from "react";
-import {
-  ConnectButton,
-  useActiveAccount,
-} from "thirdweb/react";
+ import { useState } from "react";
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
 import BuyModal from "../components/BuyModal";
 
 export default function Home() {
@@ -12,27 +9,16 @@ export default function Home() {
   return (
     <div style={{ padding: 32 }}>
       <h1>MAZOL MZLx Private Sale</h1>
-      <ConnectButton
-        clientId={
-          process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID
-        }
-      />
-      <button
-        onClick={() => setShowBuy(true)}
-        style={{ marginTop: 24 }}
-      >
+      <ConnectButton clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID} />
+      <button onClick={() => setShowBuy(true)} style={{ marginTop: 24 }}>
         Buy Tokens
       </button>
-      {showBuy && (
-        <BuyModal onClose={() => setShowBuy(false)} />
-      )}
+      {showBuy && <BuyModal onClose={() => setShowBuy(false)} />}
       {account && (
         <div style={{ marginTop: 16 }}>
-          Connected: {account.address.slice(0, 6)}...
-          {account.address.slice(-4)}
+          Connected: {account.address.slice(0, 6)}...{account.address.slice(-4)}
         </div>
       )}
     </div>
   );
 }
-
