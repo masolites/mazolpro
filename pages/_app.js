b651254be9e7519b0ae03f4e3dba12a2e8d20657
masolites <mfmmpcs@gmail.com>
2025-07-17 17:43:21 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,15 @@
-// pages/_app.js
 import { ThirdwebProvider } from "thirdweb/react";
-
-// Debug log to check if the env variable is available at build time
-console.log(
-  "DEBUG: NEXT_PUBLIC_THIRDWEB_CLIENT_ID =",
-  process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
-);
 
 const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
 
 export default function App({ Component, pageProps }) {
   if (!clientId) {
-    // Show a clear error in the browser if the env var is missing
     return (
       <div style={{ padding: 32, color: "red" }}>
         <h1>Configuration Error</h1>
         <p>
-          <b>NEXT_PUBLIC_THIRDWEB_CLIENT_ID</b> is not set.
-          <br />
-          Please set this environment variable in your
-          Netlify dashboard and redeploy.
+          <b>NEXT_PUBLIC_THIRDWEB_CLIENT_ID</b> is not set.<br />
+          Please set this environment variable in your Netlify dashboard and redeploy.
         </p>
       </div>
     );
