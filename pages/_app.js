627732dc567bc690b7bfe1183926586c62f77840
masolites <mfmmpcs@gmail.com>
2025-07-18 11:43:19 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
  import { ThirdwebProvider } from "thirdweb/react";
+import { useEffect, useState } from "react";
 
-const clientId = "71e20f4fe4537525ee7c766d094b27b1";
+// Use public environment variable or fallback to hardcoded value
+const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "71e20f4fe4537525ee7c766d094b27b1";
+
+// Create a wrapper component that only renders on client side
+function ClientOnly({ children }) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  return mounted ? children : null;
+}
 
 export default function App({ Component, pageProps }) {
   return (
     <ThirdwebProvider clientId={clientId}>
-      <Component {...pageProps} />
+      <ClientOnly>
+        <Component {...pageProps} />
+      </ClientOnly>
     </ThirdwebProvider>
   );
 }
