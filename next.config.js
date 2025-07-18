82837cf31aea66971f61f15352fb2fe48e06947c
masolites <mfmmpcs@gmail.com>
2025-07-18 11:50:25 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "71e20f4fe4537525ee7c766d094b27b1",
+  },
+  // Add other Next.js config options here if needed
+};
