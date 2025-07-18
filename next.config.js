5510b7bae1fc5193bbc4de51c095c447dc32dbfa
masolites <mfmmpcs@gmail.com>
2025-07-18 12:43:25 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  reactStrictMode: true,
   env: {
     NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "71e20f4fe4537525ee7c766d094b27b1",
   },
-  // Add other Next.js config options here if needed
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
