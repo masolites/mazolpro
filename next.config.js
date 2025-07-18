64aba7285112d1f37f12871610078e5b341ce559
masolites <mfmmpcs@gmail.com>
2025-07-18 16:55:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "71e20f4fe4537525ee7c766d094b27b1",
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
+  images: {
+    domains: ['checkout.flutterwave.com'],
   },
 };
