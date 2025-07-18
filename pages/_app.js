9b9b875b2a76c435b171f366a1fbb8eeb654b0b4
masolites <mfmmpcs@gmail.com>
2025-07-18 16:45:12 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,15 @@
 import { ThirdwebProvider } from "thirdweb/react";
-import { useState, useEffect } from "react";
-import Head from "next/head";
+import { WalletWrapper } from '../components/WalletWrapper';
 
-// 1. Directly use the client ID without env fallback for now
-const clientId = "71e20f4fe4537525ee7c766d094b27b1";
-
-// 2. Simplified provider wrapper
 export default function App({ Component, pageProps }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return (
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh'
-      }}>
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
   return (
-    <ThirdwebProvider clientId={clientId}>
-      <Component {...pageProps} />
+    <ThirdwebProvider
+      clientId="71e20f4fe4537525ee7c766d094b27b1"
+      activeChain="binance"
+    >
+      <WalletWrapper>
+        <Component {...pageProps} />
+      </WalletWrapper>
     </ThirdwebProvider>
   );
 }
