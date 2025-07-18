9ca227c473b62d51e608caa44cd2dfa8bd6f702e
masolites <mfmmpcs@gmail.com>
2025-07-18 11:31:42 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,8 @@
-import { ThirdwebProvider } from "thirdweb/react";
+ import { ThirdwebProvider } from "thirdweb/react";
 
-const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
+const clientId = "71e20f4fe4537525ee7c766d094b27b1";
 
 export default function App({ Component, pageProps }) {
-  if (!clientId) {
-    return (
-      <div style={{ padding: 32, color: "red" }}>
-        <h1>Configuration Error</h1>
-        <p>
-          <b>NEXT_PUBLIC_THIRDWEB_CLIENT_ID</b> is not set.
-          <br />
-          Please set this environment variable in your
-          Netlify dashboard and redeploy.
-        </p>
-      </div>
-    );
-  }
-
   return (
     <ThirdwebProvider clientId={clientId}>
       <Component {...pageProps} />
