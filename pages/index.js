3feb9dfe6010b02872f6f7489b0c8864a80e6918
masolites <mfmmpcs@gmail.com>
2025-07-18 05:28:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
- import { useState } from "react";
-import { ConnectButton, useActiveAccount } from "thirdweb/react";
+import { useState } from "react";
+import {
+  ConnectButton,
+  useActiveAccount,
+} from "thirdweb/react";
 import BuyModal from "../components/BuyModal";
+
+const THIRDWEB_CLIENT_ID =
+  "23ca42a52fded0d2d5adb5f79c92030e";
 
 export default function Home() {
   const [showBuy, setShowBuy] = useState(false);
@@ -9,14 +15,20 @@ export default function Home() {
   return (
     <div style={{ padding: 32 }}>
       <h1>MAZOL MZLx Private Sale</h1>
-      <ConnectButton clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID} />
-      <button onClick={() => setShowBuy(true)} style={{ marginTop: 24 }}>
+      <ConnectButton clientId={THIRDWEB_CLIENT_ID} />
+      <button
+        onClick={() => setShowBuy(true)}
+        style={{ marginTop: 24 }}
+      >
         Buy Tokens
       </button>
-      {showBuy && <BuyModal onClose={() => setShowBuy(false)} />}
+      {showBuy && (
+        <BuyModal onClose={() => setShowBuy(false)} />
+      )}
       {account && (
         <div style={{ marginTop: 16 }}>
-          Connected: {account.address.slice(0, 6)}...{account.address.slice(-4)}
+          Connected: {account.address.slice(0, 6)}...
+          {account.address.slice(-4)}
         </div>
       )}
     </div>
