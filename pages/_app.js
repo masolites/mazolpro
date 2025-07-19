520cb2254806c03b2273837cabd9f6fdcc323d69
masolites <mfmmpcs@gmail.com>
2025-07-19 06:26:07 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
+// /pages/_app.js
+
 import { ThirdwebProvider } from "thirdweb/react";
 import { embeddedWallet, inAppWallet } from "thirdweb/wallets";
+
+// You can add more wallets here if your SDK version supports them
+// e.g., coinbaseWallet, metamaskWallet, etc. (check your SDK docs)
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+      // Add supported wallets to this array
       wallets={[
         embeddedWallet(),
         inAppWallet(),
+        // Add more wallets here if needed and supported
       ]}
+      // You can add other props here if needed, such as activeChain, supportedChains, etc.
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
