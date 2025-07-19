104b1694fb1d97a3aeea8dce7310b609182ebd75
masolites <mfmmpcs@gmail.com>
2025-07-19 02:20:09 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,13 @@
-// pages/_app.js
-import dynamic from "next/dynamic";
-
-// Dynamically import ThirdwebProvider to ensure it's only rendered on the client
-const ThirdwebProvider = dynamic(
-  () =>
-    import("thirdweb/react").then(
-      (mod) => mod.ThirdwebProvider,
-    ),
-  { ssr: false },
-);
-
-import { WalletWrapper } from "../components/WalletWrapper";
+import { ThirdwebProvider } from "thirdweb/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
       activeChain="binance"
+      supportedWallets={["embeddedWallet", "metamask", "coinbase", "walletConnect"]}
     >
-      <WalletWrapper>
-        <Component {...pageProps} />
-      </WalletWrapper>
+      <Component {...pageProps} />
     </ThirdwebProvider>
   );
 }
