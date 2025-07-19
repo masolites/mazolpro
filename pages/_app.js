823a7dfd50ccb582bb529df717fb4cfaf50c8ddb
masolites <mfmmpcs@gmail.com>
2025-07-19 04:32:50 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 // /pages/_app.js
 
 import { ThirdwebProvider } from "thirdweb/react";
-import { injectedWallet, inAppWallet } from "thirdweb/wallets";
+import { walletConnectWallet, inAppWallet } from "thirdweb/wallets";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
       wallets={[
-        injectedWallet(), // This supports MetaMask and other browser wallets
+        walletConnectWallet(), // Supports MetaMask and other WalletConnect wallets
         inAppWallet({
           // Add your in-app wallet config here if needed
         }),
