b246f97b1c8478de4193b2602520ebcb69ee6b90
masolites <mfmmpcs@gmail.com>
2025-07-19 06:27:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+// /pages/index.js
+
 import { ConnectButton } from "thirdweb/react";
 
 export default function Home() {
   return (
-    <main>
+    <main style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: 40 }}>
       <h1>Welcome to Mazol</h1>
+      <p>
+        Create or connect your Mazol wallet to get started with buying, mining, or managing your MZLx tokens.
+      </p>
       <ConnectButton />
+      {/* You can add more UI here for token purchase, mining, etc. */}
     </main>
   );
 }
