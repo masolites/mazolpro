4b9d3bc869c67b1e931058caaac9b7ab0ff7ad1b
masolites <mfmmpcs@gmail.com>
2025-07-21 16:59:07 +0100

    Delete src directory

--- src/client.ts
+++
@@ -1,6 +0,0 @@
-import { createThirdwebClient } from "thirdweb";
-
-// Replace with your actual client ID from Thirdweb dashboard
-export const client = createThirdwebClient({ 
-  clientId: "4a380b99db921d6415e1f86369ca7df9" 
-});
